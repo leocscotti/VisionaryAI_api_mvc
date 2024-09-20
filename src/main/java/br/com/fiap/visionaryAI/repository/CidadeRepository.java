4863ab723963fe67616d44bb2de96a0322438618
Leo Scotti <126523762+leocscotti@users.noreply.github.com>
2024-09-20 11:20:21 -0300

    atualizacao

--- src/main/java/br/com/fiap/visionaryAI/repository/CidadeRepository.java
+++ src/main/java/br/com/fiap/visionaryAI/repository/CidadeRepository.java
@@ -2,6 +2,8 @@ package br.com.fiap.visionaryAI.repository;
 
 import br.com.fiap.visionaryAI.models.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 }
